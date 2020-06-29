326e25a78ce98f911e276530672ad39136e45233
Guilherme Oliveira <guilhergoa@hotmail.com>
2020-06-29 10:39:22 -0300

    fix: add files styles in screens

---
+++ src/pages/Detail/styles.ts
@@ -0,0 +1,78 @@
+import { StyleSheet } from "react-native";
+import colors from "../../utils/colors";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+    paddingTop: 20,
+  },
+
+  pointImage: {
+    width: "100%",
+    height: 120,
+    resizeMode: "cover",
+    borderRadius: 10,
+    marginTop: 32,
+  },
+
+  pointName: {
+    color: colors.secondary,
+    fontSize: 28,
+    fontFamily: "Ubuntu_700Bold",
+    marginTop: 24,
+  },
+
+  pointItems: {
+    fontFamily: "Roboto_400Regular",
+    fontSize: 16,
+    lineHeight: 24,
+    marginTop: 8,
+    color: colors.grey100,
+  },
+
+  address: {
+    marginTop: 32,
+  },
+
+  addressTitle: {
+    color: colors.secondary,
+    fontFamily: "Roboto_500Medium",
+    fontSize: 16,
+  },
+
+  addressContent: {
+    fontFamily: "Roboto_400Regular",
+    lineHeight: 24,
+    marginTop: 8,
+    color: colors.grey100,
+  },
+
+  footer: {
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderColor: colors.grey200,
+    paddingVertical: 20,
+    paddingHorizontal: 32,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  button: {
+    width: "48%",
+    backgroundColor: colors.primary,
+    borderRadius: 10,
+    height: 50,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  buttonText: {
+    marginLeft: 8,
+    color: colors.white,
+    fontSize: 16,
+    fontFamily: "Roboto_500Medium",
+  },
+});
+
+export default styles;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,6 @@ import {
   ImageBackground,
   Text,
   Image,
-  StyleSheet,
   TextInput,
   KeyboardAvoidingView,
   Platform,
@@ -12,6 +11,8 @@ import {
 import { Feather as Icon } from "@expo/vector-icons";
 import { RectButton } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
+
+import styles from "./styles";
 
 const Home = () => {
   const [uf, setUf] = useState<string>("");
@@ -80,72 +81,3 @@ const Home = () => {
 };
 
 export default Home;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 32,
-  },
-
-  main: {
-    flex: 1,
-    justifyContent: "center",
-  },
-
-  title: {
-    color: "#322153",
-    fontSize: 32,
-    fontFamily: "Ubuntu_700Bold",
-    maxWidth: 260,
-    marginTop: 64,
-  },
-
-  description: {
-    color: "#6C6C80",
-    fontSize: 16,
-    marginTop: 16,
-    fontFamily: "Roboto_400Regular",
-    maxWidth: 260,
-    lineHeight: 24,
-  },
-
-  footer: {},
-
-  select: {},
-
-  input: {
-    height: 60,
-    backgroundColor: "#FFF",
-    borderRadius: 10,
-    marginBottom: 8,
-    paddingHorizontal: 24,
-    fontSize: 16,
-  },
-
-  button: {
-    backgroundColor: "#34CB79",
-    height: 60,
-    flexDirection: "row",
-    borderRadius: 10,
-    overflow: "hidden",
-    alignItems: "center",
-    marginTop: 8,
-  },
-
-  buttonIcon: {
-    height: 60,
-    width: 60,
-    backgroundColor: "rgba(0, 0, 0, 0.1)",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  buttonText: {
-    flex: 1,
-    justifyContent: "center",
-    textAlign: "center",
-    color: "#FFF",
-    fontFamily: "Roboto_500Medium",
-    fontSize: 16,
-  },
-});
