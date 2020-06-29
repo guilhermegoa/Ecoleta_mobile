b8e45f40fdc9c77671235561474d21b85fb847b7
Guilherme Oliveira <guilhergoa@hotmail.com>
2020-06-29 10:50:04 -0300

    fix: add colors

---
+++ src/utils/colors.ts
@@ -0,0 +1,13 @@
+const colors = {
+  primary: "#34CB79",
+
+  secondary: "#322153",
+
+  grey100: "#EEE",
+  grey200: "#6C6C80",
+  grey300: "#999",
+
+  white: "#FFF",
+};
+
+export default colors;
